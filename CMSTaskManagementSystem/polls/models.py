3cfdcc5a79093d763c8bb3f6c5c11041e5656aa0
rsamulski <85201028+rsamulski@users.noreply.github.com>
2023-06-20 16:02:28 +0200

    Update models.py

--- CMSTaskManagementSystem/polls/models.py
+++ CMSTaskManagementSystem/polls/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Boards(models.Model):
+    name = models.CharField(max_length=100)
+    opendate = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Tasks(models.Model):
+    fcolumn = models.ForeignKey('Boards', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    state = models.DecimalField(max_digits=1, decimal_places=0)
+    #asigneduser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, default=None)
+
+class Asigments(models.Model):
+    bid = models.ForeignKey('Boards', on_delete=models.CASCADE)
+    uid = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
