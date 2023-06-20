9479c7032111d6b8112f35d71504789269536924
rsamulski <85201028+rsamulski@users.noreply.github.com>
2023-06-20 16:01:56 +0200

    Update views

--- CMSTaskManagementSystem/polls/views.py
+++ CMSTaskManagementSystem/polls/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from polls.models import Boards, Tasks, Asigments
+import logging
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from builtins import print
+from .models import Boards
+
+logger = logging.getLogger(__name__)
+
+def home(request):
+    
+    return render(request, 'polls/home.html')
+
+@login_required
+def board(request):
+    current_user = request.user
+    boards = Boards.objects.filter(creator=current_user)
+
+    return render(request, 'polls/board.html', {'boards': boards})
 
 
-def index(request):
-    return render(request, 'polls/index.html')
+
+class CustomLoginView(LoginView):
+    template_name = 'polls/login.html'
